9bdca3a02c6b9bbe85b5a4faede530cce5f23432
Dimitri Harding <vaughnharding@hotmail.com>
2022-07-10 12:55:03 -0500

    feat: fetch config from inline index.html

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -6,6 +6,7 @@ import {
   HamburgerIcon,
   SpinnerIcon,
 } from "@chakra-ui/icons";
+import { Spinner } from "@chakra-ui/react";
 import Iframe from "react-iframe";
 import { DiffLayout } from "./components/DiffLayout";
 import { Header } from "./components/Header";
@@ -13,7 +14,6 @@ import { DashboardCardsWithIcon } from "./components/DashboardCardsWithIcon";
 import { DashboardMetadataCards } from "./components/DasboardMetadataCards";
 import { Panels } from "./components/Panels";
 import { GeneratedAt } from "./components/GeneratedAt";
-import { fetchData } from "./utils";
 
 interface IConfig {
   title?: string;
@@ -31,122 +31,142 @@ interface IConfig {
   tests: [];
 }
 
+function isEmptyObject(obj: Object) {
+  return JSON.stringify(obj) === "{}";
+}
+
 function App() {
   const [config, setConfig] = useState<IConfig | null>(null);
 
   useEffect(() => {
-    fetchData("/config.json", (data: any) => setConfig(data as IConfig));
+    const data = window!.document!.getElementById("config")!.innerHTML;
+    const configData = JSON.parse(data);
+    setConfig(configData as IConfig);
   }, []);
+
+  if (!config) {
+    return (
+      <Stack align="center" justify="center" spacing={8}>
+        <Spinner size={"lg"} />
+      </Stack>
+    );
+  }
+
+  if (isEmptyObject(config)) {
+    return (
+      <Stack textColor={"blue.400"} align="center" justify="center" spacing={8}>
+        No config found
+      </Stack>
+    );
+  }
 
   return (
     <>
-      {config && (
-        <Stack minH="100vh" minW="100vw">
-          <Header title={config?.title || "Regression Tests"} />
-          <GeneratedAt date={config?.createdAt} />
-          <Panels
-            tabs={[
-              {
-                title: "Diff Report",
-                panel: (
-                  <Stack spacing={5}>
-                    <DashboardCardsWithIcon
-                      noShadow={false}
-                      items={[
-                        {
-                          title: "Tests",
-                          value: config?.totalTests,
-                          icon: HamburgerIcon,
-                          color: "blue",
-                        },
-                        {
-                          title: "Passes",
-                          value: config?.totalTests - config?.totalDiffs,
-                          icon: CheckCircleIcon,
-                          color: "green",
-                        },
-                        {
-                          title: "Diffs",
-                          value: config?.totalDiffs,
-                          icon: WarningIcon,
-                          color: "red",
-                        },
-                        {
-                          title: "Endpoints",
-                          value: config?.totalEndpoints,
-                          icon: SpinnerIcon,
-                          color: "orange",
-                        },
-                      ]}
-                    />
-                    <DashboardMetadataCards
-                      items={[
-                        {
-                          title: "Expected Metadata",
-                          items: [
-                            {
-                              name: "Environment",
-                              value: config?.metadata?.expected?.env,
-                            },
-                            {
-                              name: "URL",
-                              value: config?.metadata?.expected?.url,
-                            },
-                          ],
-                        },
-                        {
-                          title: "Actual Metadata",
-                          items: [
-                            {
-                              name: "Environment",
-                              value: config?.metadata?.actual?.env,
-                            },
-                            {
-                              name: "URL",
-                              value: config?.metadata?.actual?.url,
-                            },
-                          ],
-                        },
-                      ]}
-                    />
-                    <HStack mt={5} justify={"center"}>
-                      <DiffLayout diffResults={config?.tests} />
-                    </HStack>
-                  </Stack>
-                ),
-              },
-              {
-                title: "Expected HTML Cucumber Report",
-                panel: (
-                  <Iframe
-                    url="/expected_cucumber_report.html"
-                    width="100%"
-                    height="800px"
-                    id="myId"
-                    className="myClassname"
-                    display="block"
-                    position="relative"
+      <Stack minH="100vh" minW="100vw">
+        <Header title={config?.title || "Regression Tests"} />
+        <GeneratedAt date={config?.createdAt} />
+        <Panels
+          tabs={[
+            {
+              title: "Diff Report",
+              panel: (
+                <Stack spacing={5}>
+                  <DashboardCardsWithIcon
+                    noShadow={false}
+                    items={[
+                      {
+                        title: "Tests",
+                        value: config?.totalTests,
+                        icon: HamburgerIcon,
+                        color: "blue",
+                      },
+                      {
+                        title: "Passes",
+                        value: config?.totalTests - config?.totalDiffs,
+                        icon: CheckCircleIcon,
+                        color: "green",
+                      },
+                      {
+                        title: "Diffs",
+                        value: config?.totalDiffs,
+                        icon: WarningIcon,
+                        color: "red",
+                      },
+                      {
+                        title: "Endpoints",
+                        value: config?.totalEndpoints,
+                        icon: SpinnerIcon,
+                        color: "orange",
+                      },
+                    ]}
                   />
-                ),
-              },
-              {
-                title: "Actual HTML Cucumber Report",
-                panel: (
-                  <Iframe
-                    url="/actual_cucumber_report.html"
-                    width="100%"
-                    height="800px"
-                    id="myId"
-                    className="myClassname"
-                    display="block"
-                    position="relative"
+                  <DashboardMetadataCards
+                    items={[
+                      {
+                        title: "Expected Metadata",
+                        items: [
+                          {
+                            name: "Environment",
+                            value: config?.metadata?.expected?.env,
+                          },
+                          {
+                            name: "URL",
+                            value: config?.metadata?.expected?.url,
+                          },
+                        ],
+                      },
+                      {
+                        title: "Actual Metadata",
+                        items: [
+                          {
+                            name: "Environment",
+                            value: config?.metadata?.actual?.env,
+                          },
+                          {
+                            name: "URL",
+                            value: config?.metadata?.actual?.url,
+                          },
+                        ],
+                      },
+                    ]}
                   />
-                ),
-              },
-            ]}
-          />
-        </Stack>
-      )}
+                  <HStack mt={5} justify={"center"}>
+                    <DiffLayout diffResults={config?.tests} />
+                  </HStack>
+                </Stack>
+              ),
+            },
+            {
+              title: "Expected HTML Cucumber Report",
+              panel: (
+                <Iframe
+                  url="/expected_cucumber_report.html"
+                  width="100%"
+                  height="800px"
+                  id="myId"
+                  className="myClassname"
+                  display="block"
+                  position="relative"
+                />
+              ),
+            },
+            {
+              title: "Actual HTML Cucumber Report",
+              panel: (
+                <Iframe
+                  url="/actual_cucumber_report.html"
+                  width="100%"
+                  height="800px"
+                  id="myId"
+                  className="myClassname"
+                  display="block"
+                  position="relative"
+                />
+              ),
+            },
+          ]}
+        />
+      </Stack>
     </>
   );
 }
